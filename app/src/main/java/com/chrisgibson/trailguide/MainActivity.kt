3ebb8ec240c5f5e315c5914568024cfe63c50ef9
chrisgibson <chris.gibson83@outlook.com>
2018-04-15 10:56:08 +1000

    main activity toolbar

--- app/src/main/java/com/chrisgibson/trailguide/MainActivity.kt
+++ app/src/main/java/com/chrisgibson/trailguide/MainActivity.kt
@@ -2,11 +2,26 @@ package com.chrisgibson.trailguide
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuInflater
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            // Note that the Toolbar defined in the layout has the id "my_toolbar"
+            setSupportActionBar(findViewById(R.id.toolbar))
+        }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
+
+
 }
