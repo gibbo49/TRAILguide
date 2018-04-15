67953497ecb7031557dbc041197620e614ee4973
chrisgibson <chris.gibson83@outlook.com>
2018-04-15 18:22:17 +1000

    item adapter for recyclerview

--- app/src/main/java/com/chrisgibson/trailguide/Controller/CategoryItemsActivity.kt
+++ app/src/main/java/com/chrisgibson/trailguide/Controller/CategoryItemsActivity.kt
@@ -1,13 +1,30 @@
 package com.chrisgibson.trailguide.Controller
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.chrisgibson.trailguide.Adapter.ItemAdapter
 import com.chrisgibson.trailguide.R
+import com.chrisgibson.trailguide.Services.DataService
+import com.chrisgibson.trailguide.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_category_items.*
 
 class CategoryItemsActivity : AppCompatActivity() {
 
+    lateinit var adapter : ItemAdapter
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category_items)
+        val selectedCategory = intent.getStringExtra(EXTRA_CATEGORY)
+        selectedCategoryText.text = "$selectedCategory GEAR"
+
+        adapter = ItemAdapter(this, DataService.getItems(selectedCategory)) {item ->
+            val itemDetailIntent = Intent(this, ItemDetailActivity::class.java)
+            startActivity(itemDeatilIntent)
+
+        }
     }
 }
