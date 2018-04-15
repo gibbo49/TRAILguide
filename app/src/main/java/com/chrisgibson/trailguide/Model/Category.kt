16b5cb82ae81c0f4be8969a5a9f661e701018da8
chrisgibson <chris.gibson83@outlook.com>
2018-04-15 17:03:11 +1000

    main activity recycler view list

---
+++ app/src/main/java/com/chrisgibson/trailguide/Model/Category.kt
@@ -0,0 +1,7 @@
+package com.chrisgibson.trailguide.Model
+
+class Category(val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
---
+++ app/src/main/java/com/chrisgibson/trailguide/Services/DataService.kt
@@ -0,0 +1,15 @@
+package com.chrisgibson.trailguide.Services
+
+import com.chrisgibson.trailguide.Model.Category
+
+object DataService {
+
+    val categories = listOf(
+            Category("HIKING", "hiking"),
+            Category("CAMPING", "camping"),
+            Category("FISHING", "fishing"),
+            Category("SURFING", "surfing"),
+            Category("DIVING", "diving")
+
+    )
+}
