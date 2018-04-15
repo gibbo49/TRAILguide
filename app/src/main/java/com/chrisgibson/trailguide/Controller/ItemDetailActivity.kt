b60dc5166549550d48e7c2c09777027104561482
chrisgibson <chris.gibson83@outlook.com>
2018-04-15 18:29:05 +1000

    item recyclerview

---
+++ app/src/main/java/com/chrisgibson/trailguide/Controller/ItemDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.chrisgibson.trailguide.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.chrisgibson.trailguide.R
+
+class ItemDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_detail)
+    }
+}
