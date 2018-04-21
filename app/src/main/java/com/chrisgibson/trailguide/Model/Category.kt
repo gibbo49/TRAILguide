4bdf6f2507c2b8c4e2ffafa04211d541df226538
chrisgibson <chris.gibson83@outlook.com>
2018-04-21 11:31:44 +1000

    added experimental to gradle and parcelize to class's

--- app/src/main/java/com/chrisgibson/trailguide/Model/Category.kt
+++ app/src/main/java/com/chrisgibson/trailguide/Model/Category.kt
@@ -1,34 +1,8 @@
 package com.chrisgibson.trailguide.Model
 
-import android.os.Parcel
 import android.os.Parcelable
-
-class Category(val title: String, val image: String, val url: String) : Parcelable {
-    constructor(parcel: Parcel) : this(
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readString()) {
-    }
+import kotlinx.android.parcel.Parcelize
 
 
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(title)
-        parcel.writeString(image)
-        parcel.writeString(url)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Category> {
-        override fun createFromParcel(parcel: Parcel): Category {
-            return Category(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Category?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+@Parcelize
+class Category(val title: String, val image: String, val url: String) : Parcelable
