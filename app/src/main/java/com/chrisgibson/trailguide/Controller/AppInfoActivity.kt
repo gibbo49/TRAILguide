7abe5ec710f8d6afe0943b3eba1c7488db5d6699
chrisgibson <chris.gibson83@outlook.com>
2018-04-23 16:33:11 +1000

    refactor and add progress bar on main activity->categoryactivity due to delay

--- app/src/main/java/com/chrisgibson/trailguide/Controller/AppInfoActivity.kt
+++ app/src/main/java/com/chrisgibson/trailguide/Controller/AppInfoActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.chrisgibson.trailguide.R
 import kotlinx.android.synthetic.main.activity_app_info.*
 
@@ -12,16 +13,16 @@ class AppInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_info)
+    }
 
-        animation_view.setOnClickListener {
-            val link = Uri.parse("http://www.devslopes.com")
-            val browser = Intent(Intent.ACTION_VIEW, link)
-            startActivity(browser)
-        }
+    fun animationClicked (view:View){
+        val link = Uri.parse("http://www.devslopes.com")
+        val browser = Intent(Intent.ACTION_VIEW, link)
+        startActivity(browser)
+    }
 
-        appInfoBackBtn.setOnClickListener {
-            val appInfoBackIntent = Intent(this, MainActivity::class.java)
-            startActivity(appInfoBackIntent)
-        }
+    fun appInfoBackClicked (view:View){
+        val appInfoBackIntent = Intent(this, MainActivity::class.java)
+        startActivity(appInfoBackIntent)
     }
 }
--- app/src/main/java/com/chrisgibson/trailguide/Controller/MainActivity.kt
+++ app/src/main/java/com/chrisgibson/trailguide/Controller/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import com.chrisgibson.trailguide.Adapter.CategoryAdapter
 import com.chrisgibson.trailguide.R
 import com.chrisgibson.trailguide.Services.DataService
@@ -20,13 +21,10 @@ class MainActivity : AppCompatActivity() {
             setContentView(R.layout.activity_main)
             setSupportActionBar(findViewById(R.id.toolbar))
 
-            appInfo_btn.setOnClickListener {
-                val appInfoIntent = Intent(this, AppInfoActivity::class.java)
-                startActivity(appInfoIntent)
-            }
             adapter = CategoryAdapter(this, DataService.categories){category ->
                 val categoryItemsIntent = Intent(this,CategoryItemsActivity::class.java)
                 categoryItemsIntent.putExtra(EXTRA_CATEGORY,category.title)
+                mainprogressBar.visibility = View.VISIBLE
                 startActivity(categoryItemsIntent)
             }
             categoryListView.adapter = adapter
@@ -35,4 +33,15 @@ class MainActivity : AppCompatActivity() {
             categoryListView.layoutManager = layoutManager
             categoryListView.setHasFixedSize(true)
         }
+
+        fun appInfoBtnClicked (view:View){
+            val appInfoIntent = Intent(this, AppInfoActivity::class.java)
+            startActivity(appInfoIntent)
+        }
+
+    override fun onResume() {
+        super.onResume()
+        mainprogressBar.visibility = View.INVISIBLE
+    }
+
 }
