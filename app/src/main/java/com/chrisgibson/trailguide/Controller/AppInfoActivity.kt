4ced4e8220a11cf9f258267736db717daa2ec654
chrisgibson <chris.gibson83@outlook.com>
2018-04-27 19:55:26 +1000

    longclicklistener on appinfoactivity

--- app/src/main/java/com/chrisgibson/trailguide/Controller/AppInfoActivity.kt
+++ app/src/main/java/com/chrisgibson/trailguide/Controller/AppInfoActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat.startActivity
 import android.view.View
+import android.widget.Toast
 import com.chrisgibson.trailguide.R
 import kotlinx.android.synthetic.main.activity_app_info.*
 
@@ -13,12 +15,17 @@ class AppInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_info)
-    }
 
-    fun animationClicked (view:View){
-        val link = Uri.parse("http://www.devslopes.com")
-        val browser = Intent(Intent.ACTION_VIEW, link)
-        startActivity(browser)
+            animation_view.setOnClickListener(View.OnClickListener {
+                animation_view.playAnimation()
+                Toast.makeText(this, "Click and hold to go to DEVSLOPES.COM.", Toast.LENGTH_LONG).show()
+            })
+
+            animation_view.setOnLongClickListener(View.OnLongClickListener {
+                val link = Uri.parse("http://www.devslopes.com")
+                val browser = Intent(Intent.ACTION_VIEW, link)
+                startActivity(browser)
+                true})
     }
 
     fun appInfoBackClicked (view:View){
